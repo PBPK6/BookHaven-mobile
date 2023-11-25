eebc1bb86b4bbded859f351facf63ed10252ccea
cyll0 <c.nagle744@gmail.com>
2023-11-25 18:00:14 +0700

    Updated NavBar

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:bookhaven_mobile/screens/home.dart';
 import 'package:bookhaven_mobile/screens/top.dart';
 import 'package:bookhaven_mobile/screens/library.dart';
@@ -7,36 +6,13 @@ import 'package:bookhaven_mobile/screens/booklist.dart';
 import 'package:bookhaven_mobile/screens/reviews.dart';
 
 
-class NavigationMenu extends StatelessWidget {
-  const NavigationMenu({super.key});
-
+class NavigationMenu extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    final controller = Get.put(NavigationController());
-    return Scaffold(
-      bottomNavigationBar: Obx(
-        () =>  NavigationBar(
-          height: 80,
-          elevation: 0,
-          selectedIndex: controller.selectedIndex.value,
-          onDestinationSelected: (index) => controller.selectedIndex.value = index,
-          destinations: [
-            const NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            const NavigationDestination(icon: Icon(Icons.recommend), label: 'Top'),
-            const NavigationDestination(icon: Icon(Icons.library_books_rounded), label: 'Library'),
-            const NavigationDestination(icon: Icon(Icons.list), label: 'Booklist'),
-            const NavigationDestination(icon: Icon(Icons.reviews), label: 'Reviews')
-          ],
-        ),
-      ),
-      body: Obx(() => controller.screens[controller.selectedIndex.value]),
-    );
-  }
+  _NavigationMenuState createState() => _NavigationMenuState();
+  const NavigationMenu({super.key});
 }
-
-class NavigationController extends GetxController{
-  final Rx<int> selectedIndex = 0.obs;
-
+class _NavigationMenuState extends State<NavigationMenu> {
+  int index = 0;
   final screens = [
     const HomePage(),
     const TopPage(),
@@ -44,4 +20,55 @@ class NavigationController extends GetxController{
     const BookListPage(),
     const ReviewsPage(),
     ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[index],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor:Colors.blue.shade100, 
+          labelTextStyle: MaterialStateProperty.all(
+            const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+          ),
+        ),
+        child: NavigationBar(
+          height: 60,
+          backgroundColor: Colors.white,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          selectedIndex: index,
+          animationDuration: const Duration(milliseconds: 500),
+          onDestinationSelected: (index) =>
+            setState(() => this.index = index),
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              selectedIcon: Icon(Icons.home_filled),
+              label: 'Home'
+              ),
+            NavigationDestination(
+              icon: Icon(Icons.recommend),
+              selectedIcon: Icon(Icons.recommend),
+              label: 'Top'
+              ),
+            NavigationDestination(
+              icon: Icon(Icons.library_books_rounded), 
+              selectedIcon: Icon(Icons.library_books_rounded),
+              label: 'Library'
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.list), 
+              selectedIcon: Icon(Icons.list),
+              label: 'Booklist'
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.reviews), 
+              selectedIcon: Icon(Icons.reviews),
+              label: 'Reviews'
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
