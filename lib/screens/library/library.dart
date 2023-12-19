6876e08105ceacc1b9f10309bd4d0c3e7530ce33
alfian-f <alfian.0615@gmail.com>
2023-12-19 10:14:22 +0700

    book widget

--- lib/screens/library/library.dart
+++ lib/screens/library/library.dart
@@ -1,12 +1,13 @@
-import 'package:bookhaven_mobile/widgets/left_drawer.dart';
 import 'package:flutter/material.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:bookhaven_mobile/widgets/left_drawer.dart';
 import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:http/http.dart' as http;
 import 'dart:html' as html;
 import 'dart:convert';
 import 'package:bookhaven_mobile/models/Book.dart';
 import 'package:bookhaven_mobile/screens/library/book_detail_page.dart';
+import 'package:bookhaven_mobile/widgets/book_card.dart';
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({Key? key}) : super(key: key);
@@ -37,19 +38,11 @@ class _LibraryPageState extends State<LibraryPage> {
   Future<bool> isAdmin() async {
     final request = context.watch<CookieRequest>();
     final response = await request.get('http://127.0.0.1:8000/check_su/');
-
-    print(response);
     return response['is_superuser'];
-  }
-
-  void _refreshList() {
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final request = context.watch<CookieRequest>();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Library'),
@@ -65,149 +58,64 @@ class _LibraryPageState extends State<LibraryPage> {
             return Center(
               child: Text('Error: ${snapshot.error}'),
             );
-          } else if (!snapshot.hasData || snapshot.data![0].isEmpty) {
-            return const Center(
-              child: Text(
-                "No book data available.",
-                style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-              ),
-            );
           } else {
             List<Book> books = snapshot.data![0];
             bool isAdmin = snapshot.data![1];
+            print(isAdmin);
 
-            return ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (_, index) => InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BookDetailPage(
-                        book: books[index],
-                      ),
-                    ),
-                  );
-                },
-                child: Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 12,
+            if (isAdmin) {
+              return Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _openLinkInBrowser(
+                          'http://127.0.0.1:8000/admin/main/book/');
+                    },
+                    child: Text('Edit'),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        Hero(
-                          tag: "book-${books[index].fields.isbn}",
-                          child: Image.network(
-                            "${books[index].fields.imageL}",
-                            width: 80,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "${books[index].fields.title}",
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Text("${books[index].fields.author}"),
-                              const SizedBox(height: 10),
-                              Text("${books[index].fields.year}")
-                            ],
-                          ),
-                        ),
-                        const SizedBox(width: 16),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Column(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () async {
-                                  final response = await request.postJson(
-                                      "http://127.0.0.1:8000/add_to_list_fl/",
-                                      jsonEncode(<String, dynamic>{
-                                        'isbn': "${books[index].fields.isbn}",
-                                      }));
-                                  if (response['status'] == 'success') {
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(const SnackBar(
-                                      content: Text("Book added successfully!"),
-                                    ));
-                                  } else {
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(const SnackBar(
-                                      content: Text(
-                                          "Something went wrong, please try again."),
-                                    ));
-                                  }
-                                },
-                                child: Text('Add'),
-                              ),
-                              const SizedBox(height: 4.0),
-                              if (isAdmin)
-                                ElevatedButton(
-                                  onPressed: () async {
-                                    final response = await request.postJson(
-                                      "http://127.0.0.1:8000/del_from_library_fl/",
-                                      jsonEncode(<String, dynamic>{
-                                        'isbn': "${books[index].fields.isbn}",
-                                      }),
-                                    );
-                                    if (response['status'] == 'success') {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(const SnackBar(
-                                        content:
-                                            Text("Book removed successfully!"),
-                                      ));
+                  Expanded(
+                    child: _buildBookList(books),
+                  ),
+                ],
+              );
+            }
 
-                                      // Refresh the list after successful deletion
-                                      _refreshList();
-                                    } else {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(const SnackBar(
-                                        content: Text(
-                                            "Something went wrong, please try again."),
-                                      ));
-                                    }
-                                  },
-                                  child: Text('Remove'),
-                                ),
-                              const SizedBox(height: 4.0),
-                              if (isAdmin)
-                                ElevatedButton(
-                                  onPressed: () {
-                                    _openLinkInBrowser(
-                                        'http://127.0.0.1:8000/admin/main/book/');
-                                  },
-                                  child: Text('Edit'),
-                                ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
+            return _buildBookList(books);
           }
         },
       ),
     );
   }
 
+  Widget _buildBookList(List<Book> books) {
+    if (books.isEmpty) {
+      return Center(
+        child: Text(
+          "No book data available.",
+          style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: books.length,
+      itemBuilder: (_, index) => InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => BookDetailPage(
+                book: books[index],
+              ),
+            ),
+          );
+        },
+        child: BookCard(books[index]),
+      ),
+    );
+  }
+
   void _openLinkInBrowser(String url) {
-    html.window.open(url, 'New Tab');
+    html.window.open(url, 'new_tab');
   }
 }
---
+++ lib/widgets/book_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:bookhaven_mobile/models/Book.dart';
+import 'dart:convert';
+
+class BookCard extends StatelessWidget {
+  final Book book;
+
+  const BookCard(this.book, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 12,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Hero(
+              tag: "book-${book.fields.isbn}",
+              child: Image.network(
+                "${book.fields.imageL}",
+                width: 80,
+                height: 120,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${book.fields.title}",
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text("${book.fields.author}"),
+                  const SizedBox(height: 10),
+                  Text("${book.fields.year}")
+                ],
+              ),
+            ),
+            const SizedBox(width: 16),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      final response = await request.postJson(
+                        "http://127.0.0.1:8000/add_to_list_fl/",
+                        jsonEncode(<String, dynamic>{
+                          'isbn': "${book.fields.isbn}",
+                        }),
+                      );
+                      if (response['status'] == 'success') {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Book added successfully!"),
+                        ));
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content:
+                              Text("Something went wrong, please try again."),
+                        ));
+                      }
+                    },
+                    child: Text('Add'),
+                  ),
+                  const SizedBox(height: 4.0),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
