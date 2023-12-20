405ad20f75fc977f69b750fde33ee934be5e7c7c
goosetea04 <gusti.fatu@gmail.com>
2023-12-20 17:13:04 +0700

    99% done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,93 +12,127 @@ class HomePage extends StatelessWidget {
     double maxWidth = MediaQuery.of(context).size.width - 50.0;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      ),
-      drawer: const LeftDrawer(),
-      backgroundColor: Color(0xFFFFF0CE),
-      body: FutureBuilder(
-        future: request.get(
-            'https://bookhaven-k6-tk.pbp.cs.ui.ac.id/get_user_books_count/'),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else {
-            final bookCount = snapshot.data['book_count'];
-            return Center(
-              child: Align(
-                alignment: Alignment(0, -0.9),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: maxWidth),
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        drawer: const LeftDrawer(),
+        backgroundColor: Color(0xFFFFF0CE),
+        body: Column(children: [
+          FutureBuilder(
+            future: request.get(
+                'https://bookhaven-k6-tk.pbp.cs.ui.ac.id/get_user_books_count/'),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              } else {
+                final bookCount = snapshot.data['book_count'];
+                return Center(
+                  child: Align(
+                    alignment: Alignment(0, -0.9),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: maxWidth),
+                      child: Card(
+                        color: Color(0xFFFFC436),
+                        margin: const EdgeInsets.all(16.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text(
+                                  'Hello! You currently have',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.book,
+                                      color: Colors.black,
+                                      size:
+                                          48, // You can adjust the size as needed
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      '$bookCount books!',
+                                      style: const TextStyle(
+                                        fontSize: 48,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  bookCount == 0
+                                      ? "Why do you have 0 books :( Add some now! :)"
+                                      : "Insert more books to your list now!",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 26,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+          Center(
+            child: Align(
+              alignment: Alignment(0, -0.9),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: maxWidth),
+                child: SingleChildScrollView(
                   child: Card(
                     color: Color(0xFFFFC436),
                     margin: const EdgeInsets.all(16.0),
                     child: Padding(
                       padding: const EdgeInsets.all(16.0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Text(
-                              'Hello! You currently have',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 30,
-                              ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            'About Us: ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 42,
                             ),
-                            const SizedBox(height: 8),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.book,
-                                  color: Colors.black,
-                                  size: 48, // You can adjust the size as needed
-                                ),
-                                const SizedBox(width: 8),
-                                Text(
-                                  '$bookCount books!',
-                                  style: const TextStyle(
-                                    fontSize: 48,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ],
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            // Your long text here...
+                            "In BookHaven, you can check out books from our extensive library! Like a book? Give it a rating to make sure other people would want to read! Browse, Search, Explore, and Comment! Welcome to BookHaven!",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
                             ),
-                            const SizedBox(height: 8),
-                            Text(
-                              bookCount == 0
-                                  ? "Why do you have 0 books :( Add some now! :)"
-                                  : "Insert more books to your list now!",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 26,
-                              ),
-                            ),
-                            Text(
-                                "\nAbout Us:\nIn a world driven by technology, the art of storytelling should be easily accessible to all. This is the vision that sparked the creation of 'Book Haven.' The founders, a diverse group of readers, writers, and tech enthusiasts, shared a common passion for literature. They envisioned a platform where writers could freely share their stories, and readers could explore a vast collection of books tailored to their tastes. Thus, 'Book Haven' was born."
-                                "'Book Haven' isn't just a platform; it's a sanctuary for bibliophiles. Writers can showcase their literary creations, providing detailed descriptions to entice potential readers. Users, on the other hand, can easily find books that match their preferred genres, thanks to the innovative genre recommendation system."
-                                "Reviews, although a challenge, were deemed essential to ensure quality content and provide constructive feedback to authors. The team was up for the task, determined to implement this feature in a way that would enrich the reading experience for all. As the development progressed, the team introduced a range of features aimed at enhancing user engagement. From the 'Book Haven’s recommended Books' to the 'About Us' page, every element of the app was designed to foster a sense of community and appreciation for literature."
-                                "To cater to different roles within the platform, the team established three distinct user types: Administrators and readers. Administrators would oversee the entire operation, ensuring a safe and enriching environment for all. Readers would have the power to explore and engage with the content."
-                                "'Book Haven' wasn't just an assignment; it was a passion project. The team's dedication to promoting literacy and storytelling was evident in every line of code and every design choice. They believed that in a world inundated with information, a platform dedicated to the art of storytelling was not only necessary but vital."
-                                "And so, 'Book Haven' was ready to embark on its journey, inviting writers, readers, and enthusiasts from all walks of life to join them in their literary haven. Together, they would create a space where stories thrived, and the love for books knew no bounds.")
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
               ),
-            );
-          }
-        },
-      ),
-    );
+            ),
+          ),
+        ]));
   }
 }
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -71,117 +71,102 @@ class _RegisterPageState extends State<RegisterPage> {
             ],
           ),
         ),
-        Expanded(
+        Align(
+            alignment: Alignment(0, 0),
             child: Padding(
-          padding: const EdgeInsets.all(32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                ),
-              ),
-              TextField(
-                controller: _fullnameController,
-                decoration: const InputDecoration(
-                  labelText: 'Full Name',
-                ),
-              ),
-              TextField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
-              ),
-              const SizedBox(height: 12.0),
-              TextField(
-                controller: _password1Controller,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 24.0),
-              TextField(
-                controller: _password2Controller,
-                decoration: const InputDecoration(
-                  labelText: 'Confirm Password',
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: () async {
-                  String username = _usernameController.text;
-                  String fullname = _fullnameController.text;
-                  String email = _emailController.text;
-                  String password1 = _password1Controller.text;
-                  String password2 = _password2Controller.text;
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
+                  ),
+                  TextField(
+                    controller: _fullnameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Full Name',
+                    ),
+                  ),
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: _password1Controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 24.0),
+                  TextField(
+                    controller: _password2Controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Confirm Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      String username = _usernameController.text;
+                      String fullname = _fullnameController.text;
+                      String email = _emailController.text;
+                      String password1 = _password1Controller.text;
+                      String password2 = _password2Controller.text;
 
-                  // Navigator.pop(context);
-                  final response = await request.postJson(
-                      "https://bookhaven-k6-tk.pbp.cs.ui.ac.id/auth/register/",
-                      jsonEncode(<String, String>{
-                        'username': username,
-                        'fullname': fullname,
-                        'email': email,
-                        'password1': password1,
-                        'password2': password2,
-                      }));
-                  print(response);
-                  if (response['status'] == true) {
-                    Navigator.pop(context);
-                    ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(
-                        SnackBar(
-                          content: Text('Register succesful.'),
-                        ),
-                      );
-                  } else {
-                    ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(
-                        SnackBar(
-                          content: Text(
-                              'Register failed, Please fix your username or password'),
-                        ),
-                      );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  minimumSize: const Size(100, 50),
-                ),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+                      // Navigator.pop(context);
+                      final response = await request.postJson(
+                          "https://bookhaven-k6-tk.pbp.cs.ui.ac.id/auth/register/",
+                          jsonEncode(<String, String>{
+                            'username': username,
+                            'fullname': fullname,
+                            'email': email,
+                            'password1': password1,
+                            'password2': password2,
+                          }));
+                      print(response);
+                      if (response['status'] == true) {
+                        Navigator.pop(context);
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(
+                              content: Text('Register succesful.'),
+                            ),
+                          );
+                      } else {
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                  'Register failed, Please fix your username or password'),
+                            ),
+                          );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(100, 50),
+                    ),
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-              const SizedBox(height: 24.0),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-                child: const Text(
-                  'Already have an account? Login here.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )),
+            )),
       ],
     ));
   }
